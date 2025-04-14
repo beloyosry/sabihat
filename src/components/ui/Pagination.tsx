540c3c1a30bcb8446f85558f631eda4513640f97
Belal Yosry <105988333+beloyosry@users.noreply.github.com>
2025-04-14 05:25:47 +0200

    Update Pagination.tsx

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -19,8 +19,7 @@ const Pagination: React.FC<PaginationProps> = ({
     itemsPerPage,
     totalItems,
 }) => {
-    const start = (currentPage - 1) * itemsPerPage + 1;
-    const end = Math.min(currentPage * itemsPerPage, totalItems);
+    
 
     const getPageNumbers = () => {
         const pages = [];
