8757d7723a6fbf0d6746668bf5dcb55be12d55ff
Belal Yosry <105988333+beloyosry@users.noreply.github.com>
2025-04-14 17:54:53 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -63,7 +63,7 @@ function AppRoutes() {
                 {/* Protected routes */}
                 <Route element={<ProtectedRoute />}>
                     {/* Dashboard wrapper */}
-                    <Route path="https://sabihat.vercel.app/" element={<DashboardWrapper />}>
+                    <Route path="/" element={<DashboardWrapper />}>
                         {/* Dynamic view routes with ID parameters */}
                         {viewRoutes.map((route, i) => (
                             <Route
